a27b0fe32d9282b5bf4fbd48eb89a6999f74a0c4
VirgoTheLord <alwinaby2004@gmail.com>
2024-09-24 12:09:50 +0530

    1st version

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -52,7 +52,7 @@ const projects = [
   },
 ];
 
-const work = () => {
+const Work = () => {
   const [project, setProject] = useState(projects[0]);
   const handleSlideChange = (swiper) => {
     //get current slide index
@@ -167,4 +167,4 @@ const work = () => {
   );
 };
 
-export default work;
+export default Work;
